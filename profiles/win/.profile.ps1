f57d2abc65dabd45a91d5a2f907d0a4f47f8fc97
hans <hansjesse@tutanota.com>
2021-05-14 10:06:59 -0700

    add profiles

---
+++ profiles/win/.profile.ps1
@@ -0,0 +1,12 @@
+# Add to Microsoft.PowerShell_profile.ps1 ($profile)
+# . ~/Workspace/hansjesse/profiles/win/.profile
+
+$PROFILE=$profile
+$SCRIPTS="~\scripts"
+$VIRTUALENVS="~\.virtualenvs"
+$WORKSPACE="~\Workspace"
+
+
+Function scripts {Set-Location -Path $SCRIPTS}
+Function env {Set-Location -Path $VIRTUALENVS}
+Function gwd {Set-Location -Path $WORKSPACE}
